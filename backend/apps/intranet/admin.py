fd4c1666a5a9b181c9524374fe16a9061659b5eb
frio <development@frio.name>
2012-07-30 10:40:20 +1200

    Added a basic admin for everything

---
+++ backend/apps/intranet/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from intranet.models import Entry, Event, Server, Tournament. FAQ
+
+admin.site.register(Entry)
+admin.site.register(Event)
+admin.site.register(Server)
+admin.site.register(Tournament)
+admin.site.register(FAQ)
