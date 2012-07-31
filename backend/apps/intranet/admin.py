55b0d21c88e39611423a7f263df2ec71e9cb4fc4
Sam Collinson <smc@wifi-172-24-41-162.uoa-wifi.auckland.ac.nz>
2012-07-31 12:51:13 +1200

    admin for bartab
    add bartab entry to intranet backend

--- backend/apps/intranet/admin.py
+++ backend/apps/intranet/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from intranet.models import Entry, Event, Server, Tournament. FAQ
+from intranet.models import Entry, Event, Server, Tournament, FAQ, BarTab
 
 admin.site.register(Entry)
 admin.site.register(Event)
 admin.site.register(Server)
 admin.site.register(Tournament)
 admin.site.register(FAQ)
+admin.site.register(BarTab)
