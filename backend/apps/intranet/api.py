b20c803acb5b081c19f64bdce82902b9268f6978
Sam Collinson <smc@wifi-172-24-41-162.uoa-wifi.auckland.ac.nz>
2012-07-31 12:44:36 +1200

    bartab backend
    models, view and api definition for bartab backend

--- backend/apps/intranet/api.py
+++ backend/apps/intranet/api.py
@@ -1,27 +1,38 @@
 from tastypie.resources import ModelResource
-from intranet.models import Entry, Event, Server, Tournament, FAQ
+from intranet.models import Entry, Event, Server, Tournament, FAQ, BarTab
+
 
 class EntryResource(ModelResource):
     class Meta:
         queryset = Entry.objects.all()
         resource_name = 'entry'
 
+
 class EventResource(ModelResource):
     class Meta:
         queryset = Event.objects.all()
         resource_name = 'event'
+
 
 class ServerResource(ModelResource):
     class Meta:
         queryset = Server.objects.all()
         resource_name = 'server'
 
+
 class FAQResource(ModelResource):
     class Meta:
         queryset = FAQ.objects.all()
         resource_name = 'faq'
 
+
 class TournamentResource(ModelResource):
     class Meta:
         queryset = Tournament.objects.all()
         resource_name = 'tournament'
+
+
+class BarTabResource(ModelResource):
+    class Meta:
+        queryset = BarTab.objects.all()
+        resource_name = 'bartab'
--- backend/apps/intranet/views.py
+++ backend/apps/intranet/views.py
@@ -1,13 +1,12 @@
-from datetime import datetime, timedelta
-
-from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest, HttpResponseForbidden
 from django.template import RequestContext
-from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404, render_to_response
 from django.conf import settings
 
-from intranet.models import Server
+from intranet.models import Server, BarTab
 from forms import AddServerForm
+
 
 @csrf_exempt
 def add_server(request):
@@ -15,25 +14,41 @@ def add_server(request):
     Adds a server to the database.  Server models use auto_now=True, so
     setting last_seen is not necessary (Django will automatically set it on
     save).
-    
+
     Using the server ModelForm allows us to perform any input validation that
-    we need to in there.  We may want to add some form of ACL 
+    we need to in there.  We may want to add some form of ACL
     (settings.ALLOWED_TO_UPDATE_SERVERS = []?) for controlling this resource.
     """
-    if request.method == "POST" and request.META["REMOTE_ADDR"] in settings.HLSW_SERVERS:
-        server = AddServerForm(request.POST)
-        if server.is_valid():
-            try:
-                old_server = Server.objects.get(address=server.cleaned_data["address"])
-                server.instance = old_server
-            except Server.DoesNotExist, e:
-                pass
-                
-            server.save()
-            return HttpResponse() # think of something more API-ish for this?
-        else:
-            return HttpResponseBadRequest()
-    elif request.method != "POST":
-        return HttpResponseNotAllowed(["POST"])
-    else:
+    if request.META["REMOTE_ADDR"] not in settings.HLSW_SERVERS:
         return HttpResponseForbidden()
+    if request.method != "POST":
+        return HttpResponseNotAllowed(["POST"])
+
+    server = AddServerForm(request.POST)
+    if server.is_valid():
+        try:
+            server.instance = Server.objects.get(address=server.cleaned_data["address"])
+        except Server.DoesNotExist:
+            pass
+
+        server.save()
+        return HttpResponse()  # think of something more API-ish for this?
+    else:
+        return HttpResponseBadRequest()
+
+
+def claim_bartab(request, code):
+    bartab = get_object_or_404(BarTab, code=code)
+
+    if bartab.claimed:
+        return render_to_response('backend/bartab_claimed.html',
+            RequestContext(request))
+    else:
+        bartab.claimed = True
+        bartab.claimant = request.META["REMOTE_ADDR"]
+        bartab.save()
+
+        return render_to_response('backend/bartab_unclaimed.html',
+            RequestContext(request,
+                {'confirmation': bartab.confirmation,
+                 'value': bartab.value}))
