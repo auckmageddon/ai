67aff30f35653b5e2fc7354574e49acb50882b1c
frio <development@frio.name>
2013-12-23 13:40:21 +1300

    Moves api to api_v1 in prep for rest framework
    In the interests of keeping things ticking over/easily testable, it's
    best to preserve this for now.

--- backend/ai/urls.py
+++ backend/ai/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from tastypie.api import Api
-from apps.intranet.api import *
+from apps.intranet.api_v1 import EntryResource, EventResource, TournamentResource
 from apps.intranet.views import IndexView
 
 admin.autodiscover()
